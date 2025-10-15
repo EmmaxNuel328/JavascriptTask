d4a4f180f38935cefebbed28fcc84357e3da0202
Olatunji Emmanuel <olatunjie335@gmail.com>
2025-10-15 08:17:43 +0100

    Add JavascriptTask

--- CharacterEInAString.js
+++
@@ -1,9 +0,0 @@
-let word = "Emmanuel"
-count = 0
-while(count < word.length){
-	if(word[count] == "e"){
-		console.log(word[count],"\t")
-		count += 1
-	}
-	
-}
---
+++ EinAstring.js
@@ -0,0 +1,8 @@
+let word = "Emmanuel"
+count = 0
+while(count < word.length){
+	if(word[count] == "e")
+	console.log(word[count],"\t")
+	count += 1
+	
+}
